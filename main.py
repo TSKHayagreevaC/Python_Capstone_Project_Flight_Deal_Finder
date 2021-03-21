29730e5b828925dffa4272c46b773f562074109e
THIRUMALA SRIKANTH HAYAGREEVA CHARYA <69465021+TSKHayagreevaC@users.noreply.github.com>
2021-03-21 17:49:23 +0530

    commit with apis except last part

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+from data_manager import DataManager
+from datetime import datetime, timedelta
+from flight_search import FlightSearch
+import flight_search
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+data_manager = DataManager()
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
 
+ORIGIN_CITY_IATA = "DEl"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if sheet_data[0]["iataCode"] == "":
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
 
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=6 * 30)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for destination in sheet_data:
+    flight = flight_search.check_flight(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
